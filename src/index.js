ddab54012712920d5435b7f2e0a7a3850f2acada
Ngoc Thinh <ngocthinh.dev@gmail.com>
2021-04-05 17:15:50 +0700

    Add cronjob make app don't sleep

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const morgan = require('morgan');
 const express = require('express');
 const cors = require('cors');
+const CronJob = require('cron').CronJob;
+
+// Make server don't sleep
+const job = new CronJob(
+	'59 * * * * *',
+	function () {
+		console.log("Cron job make app don't sleep");
+	},
+	null,
+	true,
+	'Asia/Ho_Chi_Minh'
+);
+job.start();
 
 const db = require('./config/db');
 const app = express();
 
 require('dotenv').config({ path: __dirname + '/./.env' });
 const route = require('./routes');
+const { ConnectionStates } = require('mongoose');
 
 // Config Cors
 app.use(cors());
